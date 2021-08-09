beed7999170d62faa16ef6566f713cb6e99e211e
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-08-08 23:57:34 -0400

    Adding styles, icons, and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import logo from "./images/logo.svg";
 
 const App = () => {
   return (
     <div>
+      <img src={logo} alt="Blockmaster" />
       <h1>Project Blockmaster with Reactjs</h1>
     </div>
   );
---
+++ src/icons/index.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+// Icon Search
+export const ISearch = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2.66667 12C2.66667 6.86116 6.86114 2.66669 12 2.66669C17.1389 2.66669 21.3333 6.86116 21.3333 12C21.3333 14.3307 20.464 16.46 19.0417 18.099L19.6094 18.6667H21.3333L29.3333 26.6667L26.6667 29.3334L18.6667 21.3334V19.6094L18.099 19.0417C16.46 20.4641 14.3307 21.3334 12 21.3334C6.86114 21.3334 2.66667 17.1389 2.66667 12ZM18.6667 12C18.6667 8.30233 15.6977 5.33335 12 5.33335C8.30231 5.33335 5.33334 8.30233 5.33334 12C5.33334 15.6977 8.30231 18.6667 12 18.6667C15.6977 18.6667 18.6667 15.6977 18.6667 12Z"
+      />
+    </svg>
+  );
+};
+// Icon Plus
+export const IPlus = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14.6667 14.6667V4H17.3333V14.6667H28V17.3333H17.3333V28H14.6667V17.3333H4V14.6667H14.6667Z"
+      />
+    </svg>
+  );
+};
+// Icon Play
+export const IPlay = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M25.3333 16L10.6667 25.3334V6.66669L25.3333 16Z"
+      />
+    </svg>
+  );
+};
+// Icon Menu
+export const IMenu = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M3 5C2.44772 5 2 5.44772 2 6C2 6.55228 2.44772 7 3 7H21C21.5523 7 22 6.55228 22 6C22 5.44772 21.5523 5 21 5H3ZM3 11C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H21C21.5523 13 22 12.5523 22 12C22 11.4477 21.5523 11 21 11H3ZM2 18C2 17.4477 2.44772 17 3 17H21C21.5523 17 22 17.4477 22 18C22 18.5523 21.5523 19 21 19H3C2.44772 19 2 18.5523 2 18Z"
+      />
+    </svg>
+  );
+};
+// Icon Star
+export const IStar = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M25.1084 29.3333L16.0004 23.836L6.89236 29.3333L9.30969 18.9733L1.26302 12.0026L11.859 11.104L16.0004 1.33331L20.1417 11.104L30.7377 12.0026L22.691 18.9733L25.1084 29.3333Z"
+      />
+    </svg>
+  );
+};
+// Icon Close
+export const IClose = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4.39062 6.27604L6.27604 4.39062L16 14.1146L25.724 4.39062L27.6094 6.27604L17.8854 16L27.6094 25.724L25.724 27.6094L16 17.8854L6.27604 27.6094L4.39062 25.724L14.1146 16L4.39062 6.27604Z"
+      />
+    </svg>
+  );
+};
+// Icon Back
+export const IBack = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M15.7071 6.70711C16.0976 6.31658 16.0976 5.68342 15.7071 5.29289C15.3166 4.90237 14.6834 4.90237 14.2929 5.29289L8.29289 11.2929C7.90237 11.6834 7.90237 12.3166 8.29289 12.7071L14.2929 18.7071C14.6834 19.0976 15.3166 19.0976 15.7071 18.7071C16.0976 18.3166 16.0976 17.6834 15.7071 17.2929L10.4142 12L15.7071 6.70711Z"
+      />
+    </svg>
+  );
+};
